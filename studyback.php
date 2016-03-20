86dcc47635710baebe09266605de3c8375983b7a
sheikhnasha <sheikh.nasha@gmail.com>
2016-03-21 06:06:16 +0800

    Added files via upload

--- studyback.php
+++ studyback.php
@@ -13,28 +13,25 @@
 						// Create connection
 			include ('conn.php');
 			//echo "Connected successfully";
+			$sql0 = "SELECT * FROM studyobject ORDER BY `studyobject`.`objectID` DESC "; 
+			$result = $conn->query($sql0);
+			$row = $result->fetch_assoc();
+			$newID=$row['objectID'] + 1;
+			
 			$sql = "INSERT INTO `studyobject` 
 			(`objectID`, `firstName`, `surName`, `ICNumber`, `telephone`, `DOB`, `Sex`, `height`, `weight`, `homeAddress`, `homeCity`, `homeState`, `DOD`, `deathCause`) 
-			VALUES (NULL, '$objectFN', '$objectSN', '$objectIC ', '$objectTel', '$objectDOB', '$objectSex', '$objectHeight', '$objectWeight', '$objectAdd', '$objectState', '$objectState', '$objectDOD', '$objectCause');";
+			VALUES ('$newID', '$objectFN', '$objectSN', '$objectIC ', '$objectTel', '$objectDOB', '$objectSex', '$objectHeight', '$objectWeight', '$objectAdd', '$objectState', '$objectState', '$objectDOD', '$objectCause');";
 			$result = $conn->query($sql);
-			/*
-			$sql1 = "SELECT * FROM login WHERE Email = '$signEmail'";
+			
+			$refID = $_POST['refID'];
+					$sql1 = "INSERT INTO `study` (`studyID`, `objectID`, `referreeID`, `referreeType`) VALUES (NULL, '$newID', '$refID', '');";
 			$result = $conn->query($sql1);
-			if ($result->num_rows > 0) {
-			// output data of each row
-			$row = $result->fetch_assoc();
-			$userID = $row["userID"]; 
-			}
-		
-			$sql2 = "SELECT * FROM surgeries WHERE surgeryName = '$signSurgery' ";
-			$result = $conn->query($sql2);
-			if ($result->num_rows > 0) {
-			// output data of each row
-			$row = $result->fetch_assoc();
-			$surgeryID = $row["surgeryID"]; 
-			}
-			$sql3 = "INSERT INTO `referees` (`referreeID`, `userID`, `firstName`, `surName`, `surgeryID`) VALUES (NULL, '$userID', '$signFN', '$signSN', '$surgeryID')";
-			$result = $conn->query($sql3);*/
+			
+			
+			
+			
+			
+			
 			header('Location: newStudy.php');
 			
 			?>
