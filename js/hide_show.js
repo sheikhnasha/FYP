3e8c79e1ed599937bd1bca162a242f7590a28522
sheikhnasha <sheikh.nasha@gmail.com>
2016-03-30 11:29:52 +0800

    Added files via upload

---
+++ js/hide_show.js
@@ -0,0 +1,14 @@
+				
+				function hide() {
+				document.getElementById("see_edit").style.display = "none"; 
+				document.getElementById("edit_button").style.display = "initial"; 
+				document.getElementById("hide_button").style.display = "none"; 
+				}
+				
+				
+				
+				function show() {
+				document.getElementById("see_edit").style.display = "initial"; 
+				document.getElementById("edit_button").style.display = "none"; 
+				document.getElementById("hide_button").style.display = "initial"; 
+				}
