14b9c14e86b5b81c34dc9f3a0dc3262823e92013
sheikhnasha <sheikh.nasha@gmail.com>
2016-04-02 03:57:01 +0800

    Added files via upload

---
+++ passchek.js
@@ -0,0 +1,28 @@
+function checkPass()
+{
+	
+		//Store the password field objects into variables ...
+		var pass1 = document.getElementById('inputPassword1');
+		var pass2 = document.getElementById('inputPassword2');
+		//Set the colors we will be using ...
+		var goodColor = "#fff";
+		var badColor = "#c66";
+		//Compare the values in the password field 
+		//and the confirmation field
+		if(pass1.value == pass2.value){
+			document.getElementById("myBtn").disabled = false;
+		//The passwords match. 
+		//Set the color to the good color and inform
+		//the user that they have entered the correct password 
+		pass2.style.backgroundColor = goodColor;
+     
+       
+    }else{
+        //The passwords do not match.
+        //Set the color to the bad color and
+        //notify the user.
+		document.getElementById("myBtn").disabled = true;
+        pass2.style.backgroundColor = badColor;
+             
+    }
+}  
